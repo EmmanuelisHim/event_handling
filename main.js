20bf332a9f58fb0d956836271ec0b3054e0a516d
EmmanuelisHim <114786431+EmmanuelisHim@users.noreply.github.com>
2023-02-02 19:42:50 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ var player_object="";
 function player_update(){
     fabric.Image.fromURL("player.png", function(Img){
         player_object = Img;
-        player_object.scaleTOWidth(150);
-        player_object.scaleTOHeight(140);
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
         player_object.set({
             top:player_y,
             left:player_x
@@ -100,5 +100,44 @@ function my_keydown(e){
         new_image('cloud.jpg');
         console.log("c");
     }
-    window.addEventListener("keydown", my_keydown);
+    //window.addEventListener("keydown", my_keydown);
+}
+function up()
+{
+if (player_y >=0){
+    player_y =player_y - block_image_height;
+    console.log("block image height = " + block_image_height);
+    console.log("When Up arrow key is pressed, X= "+ player_x +", Y = " +player_y);
+    canvas.remove(player_object);
+    player_update();
+}
+}
+function down()
+{
+if (player_y <=500){
+    player_y =player_y + block_image_height;
+    console.log("block image height = " + block_image_height);
+    console.log("When Down arrow key is pressed, X= "+ player_x +", Y = " +player_y);
+    canvas.remove(player_object);
+    player_update();
+}
+}
+function left()
+{
+if (player_x >0){
+    player_x =player_x - block_image_width;
+    console.log("block image height = " + block_image_height);
+    console.log("When Left arrow key is pressed, X= "+ player_x +", Y = " +player_y);
+    canvas.remove(player_object);
+    player_update();
+}
+}
+function right(){
+if (player_x <=850){
+    player_x =player_x + block_image_width;
+    console.log("block image height = " + block_image_height);
+    console.log("When Right arrow key is pressed, X= "+ player_x +", Y = " +player_y);
+    canvas.remove(player_object);
+    player_update();
+}
 }
